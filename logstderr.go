4299395affb58614576967511fb4a20241d03f94
ondi <thy321@gmail.com>
2019-11-16 12:33:36 +0300

    minor fix

--- logstderr.go
+++ logstderr.go
@@ -5,43 +5,34 @@
 package log
 
 import "os"
+import "io"
 import "fmt"
 import "time"
 
-type Stderr_t struct {
+type Stdany_t struct {
+	out io.Writer
 	datetime func() string
+}
+
+func NewStdany(out io.Writer, datetime string) Writer {
+	self := &Stdany_t{out: out}
+	if len(datetime) > 0 {
+		datetime += " "
+		self.datetime = func() string {return time.Now().Format(datetime)}
+	} else {
+		self.datetime = func() string {return ""}
+	}
+	return self
+}
+
+func (self * Stdany_t) WriteLevel(level string, format string, args ...interface{}) (int, error) {
+	return fmt.Fprintf(self.out, self.datetime() + level + " " + format + "\n", args...)
 }
 
 func NewStderr(datetime string) Writer {
-	self := &Stderr_t{}
-	if len(datetime) > 0 {
-		datetime += " "
-		self.datetime = func() string {return time.Now().Format(datetime)}
-	} else {
-		self.datetime = func() string {return ""}
-	}
-	return self
-}
-
-func (self * Stderr_t) WriteLevel(level string, format string, args ...interface{}) (int, error) {
-	return fmt.Fprintf(os.Stderr, self.datetime() + level + " " + format + "\n", args...)
-}
-
-type Stdout_t struct {
-	datetime func() string
+	return NewStdany(os.Stderr, datetime)
 }
 
 func NewStdout(datetime string) Writer {
-	self := &Stdout_t{}
-	if len(datetime) > 0 {
-		datetime += " "
-		self.datetime = func() string {return time.Now().Format(datetime)}
-	} else {
-		self.datetime = func() string {return ""}
-	}
-	return self
-}
-
-func (self * Stdout_t) WriteLevel(level string, format string, args ...interface{}) (int, error) {
-	return fmt.Fprintf(os.Stdout, self.datetime() + level + " " + format + "\n", args...)
+	return NewStdany(os.Stdout, datetime)
 }
