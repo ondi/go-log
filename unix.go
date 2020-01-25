f0adde7eb0b2c9eb04150fc948b95458aa060639
Sergey Klimkin <klimkin.sergey@wildberries.ru>
2020-01-25 17:49:01 +0300

    DupStdout added

--- unix.go
+++ unix.go
@@ -11,6 +11,20 @@ func DupStderr(filename string) (fp * os.File, err error) {
 	if fp, err = os.OpenFile(filename, os.O_WRONLY | os.O_CREATE /*| os.O_APPEND*/, 0644); err != nil {
 		return
 	}
-	err = syscall.Dup2(int(fp.Fd()), syscall.Stderr)
+	if err = syscall.Dup2(int(fp.Fd()), syscall.Stderr); err != nil {
+		fp.Close()
+		return
+	}
+	return
+}
+
+func DupStdout(filename string) (fp * os.File, err error) {
+	if fp, err = os.OpenFile(filename, os.O_WRONLY | os.O_CREATE /*| os.O_APPEND*/, 0644); err != nil {
+		return
+	}
+	if err = syscall.Dup2(int(fp.Fd()), syscall.Stdout); err != nil {
+		fp.Close()
+		return
+	}
 	return
 }
