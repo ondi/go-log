fb180b0cd946061d8352224b11532ffdca96d794
ondi <thy321@gmail.com>
2020-09-16 08:57:03 +0300

    minor fix

--- logger_test.go
+++ logger_test.go
@@ -12,7 +12,14 @@ func Example_log1() {
 	logger.AddOutput("stdout", 0, NewStdout(&DT_t{}))
 	log_file, _ := NewFileBytes("/tmp/test.log", &DT_t{}, 1024, 10)
 	logger.AddOutput("file", 0, log_file)
-	log_http := NewHttp(DefaultTransport(time.Second), 10, 1, "http://localhost", Message_t{}, time.Second, nil)
+	log_http := NewHttp(
+		10,
+		1,
+		"http://localhost",
+		Message_t{},
+		DefaultClient(DefaultTransport(time.Second), time.Second),
+		nil,
+	)
 	logger.AddOutput("http", 0, log_http)
 
 	Debug("lalala")
--- loghttp.go
+++ loghttp.go
@@ -9,7 +9,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -21,20 +21,24 @@ import (
 	"github.com/ondi/go-queue"
 )
 
-type Convert interface {
-	Convert(buf *bytes.Buffer, level string, format string, args ...interface{}) (n int, err error)
+type Convert_t interface {
+	Convert(out io.Writer, level string, format string, args ...interface{}) (n int, err error)
+}
+
+type Client_t interface {
+	Do(*http.Request) (*http.Response, error)
 }
 
 type Http_t struct {
-	q       queue.Queue
-	pool    sync.Pool
-	convert Convert
-	url     string
-	header  http.Header
-	client  *http.Client
+	q        queue.Queue
+	pool     sync.Pool
+	post_url string
+	convert  Convert_t
+	client   Client_t
+	header   http.Header
 }
 
-// this is working example for Convert interface
+// this is working example for Convert_t interface
 type Message_t struct {
 	ApplicationName string          `json:"ApplicationName"`
 	Environment     string          `json:"Environment"`
@@ -48,7 +52,7 @@ type Message_t struct {
 }
 
 // self is copy
-func (self Message_t) Convert(buf *bytes.Buffer, level string, format string, args ...interface{}) (n int, err error) {
+func (self Message_t) Convert(out io.Writer, level string, format string, args ...interface{}) (n int, err error) {
 	self.Level = level
 	if len(format) == 0 {
 		if self.Data, err = json.Marshal(args); err != nil {
@@ -64,8 +68,7 @@ func (self Message_t) Convert(buf *bytes.Buffer, level string, format string, ar
 			self.Location = fmt.Sprintf("%s:%d", path.Base(file), line)
 		}
 	}
-	err = json.NewEncoder(buf).Encode(self)
-	n = buf.Len()
+	err = json.NewEncoder(out).Encode(self)
 	return
 }
 
@@ -83,16 +86,21 @@ func DefaultTransport(timeout time.Duration) http.RoundTripper {
 	}
 }
 
-func NewHttp(tr http.RoundTripper, queue_size int, writers int, post_url string, convert Convert, timeout time.Duration, header http.Header) (self *Http_t) {
-	self = &Http_t{}
-	self.q = queue.New(queue_size)
-	self.pool = sync.Pool{New: func() interface{} { return new(bytes.Buffer) }}
-	self.convert = convert
-	self.url = post_url
-	self.header = header.Clone()
-	self.client = &http.Client{
+func DefaultClient(tr http.RoundTripper, timeout time.Duration) Client_t {
+	return &http.Client{
 		Transport: tr,
 		Timeout:   timeout,
+	}
+}
+
+func NewHttp(queue_size int, writers int, post_url string, convert Convert_t, client Client_t, header http.Header) (self *Http_t) {
+	self = &Http_t{
+		q:        queue.New(queue_size),
+		pool:     sync.Pool{New: func() interface{} { return bytes.NewBuffer(nil) }},
+		post_url: post_url,
+		convert:  convert,
+		client:   client,
+		header:   header.Clone(),
 	}
 	for i := 0; i < writers; i++ {
 		go self.writer()
@@ -119,28 +127,34 @@ func (self *Http_t) Size() int {
 }
 
 func (self *Http_t) writer() {
+	var oki int
+	var err error
+	var temp interface{}
+	var buf *bytes.Buffer
+	var req *http.Request
+	var resp *http.Response
 	for {
-		buf, ok := self.q.PopFront()
-		if ok == -1 {
+		if temp, oki = self.q.PopFront(); oki == -1 {
 			return
 		}
-		req, err := http.NewRequest("POST", self.url, buf.(*bytes.Buffer))
-		if err != nil {
-			self.pool.Put(buf)
+		buf = temp.(*bytes.Buffer)
+		if req, err = http.NewRequest(http.MethodPost, self.post_url, buf); err != nil {
+			self.pool.Put(temp)
 			fmt.Fprintf(os.Stderr, "%v ERROR: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
 			continue
 		}
 		req.Header = self.header
-		resp, err := self.client.Do(req)
-		self.pool.Put(buf)
-		if err != nil {
+		if resp, err = self.client.Do(req); err != nil {
+			self.pool.Put(temp)
 			fmt.Fprintf(os.Stderr, "%v ERROR: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
 			continue
 		}
 		if resp.StatusCode >= 400 {
-			temp, _ := ioutil.ReadAll(resp.Body)
-			fmt.Fprintf(os.Stderr, "%v ERROR: %v %s\n", time.Now().Format("2006-01-02 15:04:05"), resp.Status, temp)
+			buf.Reset()
+			buf.ReadFrom(resp.Body)
+			fmt.Fprintf(os.Stderr, "%v ERROR: %v %s\n", time.Now().Format("2006-01-02 15:04:05"), resp.Status, buf.Bytes())
 		}
+		self.pool.Put(temp)
 		resp.Body.Close()
 	}
 }
