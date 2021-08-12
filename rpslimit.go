f251b7e588724290bf65237584edc7b626eaced5
ondi <thy321@gmail.com>
2021-08-12 17:10:28 +0300

    minor fix

--- rpslimit.go
+++ rpslimit.go
@@ -22,25 +22,23 @@ func (NoRps_t) Overflow(time.Time) bool {
 }
 
 type Rps_t struct {
-	mx         sync.Mutex
-	c          *ttl_cache.Cache_t
-	evict      time.Duration
-	rps_limit  int
-	count      int
-	reserve    int
-	reserve_ts time.Time
+	mx        sync.Mutex
+	c         *ttl_cache.Cache_t
+	truncate  time.Duration
+	count     int
+	rps_limit int
 }
 
 /*
 example 1000 rps:
-ttl = 1s
-evict = 0.05s
+ttl = time.Second
+truncate = 50*time.Millisecond
 rps_limit=1000
 */
-func NewRps(ttl time.Duration, evict time.Duration, rps_limit int) (self *Rps_t) {
+func NewRps(ttl time.Duration, truncate time.Duration, rps_limit int) (self *Rps_t) {
 	self = &Rps_t{}
 	self.c = ttl_cache.New(1<<32-1, ttl, self.__evict)
-	self.evict = evict
+	self.truncate = truncate
 	self.rps_limit = rps_limit
 	return
 }
@@ -49,39 +47,33 @@ func (self *Rps_t) __evict(key interface{}, value interface{}) {
 	self.count -= value.(int)
 }
 
-func (self *Rps_t) add(ts time.Time) (count int, reserve bool) {
+func (self *Rps_t) add(ts time.Time) (count int) {
 	self.mx.Lock()
 	self.c.Create(
 		ts,
-		ts.Truncate(self.evict),
+		ts.Truncate(self.truncate),
 		func() interface{} {
+			if self.count > self.rps_limit {
+				return 0
+			}
+			self.count++
 			return 1
 		},
 		func(prev interface{}) interface{} {
+			if self.count > self.rps_limit {
+				return prev
+			}
+			self.count++
 			return prev.(int) + 1
 		},
 	)
-	if ts.Sub(self.reserve_ts) > self.c.TTL() {
-		if self.reserve < self.rps_limit {
-			self.reserve++
-			reserve = true
-		} else {
-			self.reserve = 0
-			self.reserve_ts = ts
-		}
-	}
-	self.count++
 	count = self.count
 	self.mx.Unlock()
 	return
 }
 
 func (self *Rps_t) Overflow(ts time.Time) bool {
-	count, reserve := self.add(ts)
-	if count > self.rps_limit && reserve == false {
-		return true
-	}
-	return false
+	return self.add(ts) > self.rps_limit
 }
 
 func (self *Rps_t) Size(ts time.Time) (res int) {
