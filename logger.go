23092983ca22b53eeb73068363a658146cc9c08c
ondi <thy321@gmail.com>
2022-08-07 00:28:57 +0300

    debug

--- logger.go
+++ logger.go
@@ -155,32 +155,37 @@ func (self *log_t) Trace(format string, args ...interface{}) {
 }
 
 func (self *log_t) ErrorCtx(ctx context.Context, format string, args ...interface{}) {
+	level := ContextStore(ctx, LOG_ERROR.Name, format, args...)
 	for _, v := range *(*writers_t)(atomic.LoadPointer(&self.out[LOG_ERROR.Levels[0]])) {
-		v.WriteLevel(ContextStore(ctx, LOG_ERROR.Name, format, args...), format, args...)
+		v.WriteLevel(level, format, args...)
 	}
 }
 
 func (self *log_t) WarnCtx(ctx context.Context, format string, args ...interface{}) {
+	level := ContextStore(ctx, LOG_WARN.Name, format, args...)
 	for _, v := range *(*writers_t)(atomic.LoadPointer(&self.out[LOG_WARN.Levels[0]])) {
-		v.WriteLevel(ContextStore(ctx, LOG_WARN.Name, format, args...), format, args...)
+		v.WriteLevel(level, format, args...)
 	}
 }
 
 func (self *log_t) InfoCtx(ctx context.Context, format string, args ...interface{}) {
+	level := ContextStore(ctx, LOG_INFO.Name, format, args...)
 	for _, v := range *(*writers_t)(atomic.LoadPointer(&self.out[LOG_INFO.Levels[0]])) {
-		v.WriteLevel(ContextStore(ctx, LOG_INFO.Name, format, args...), format, args...)
+		v.WriteLevel(level, format, args...)
 	}
 }
 
 func (self *log_t) DebugCtx(ctx context.Context, format string, args ...interface{}) {
+	level := ContextStore(ctx, LOG_DEBUG.Name, format, args...)
 	for _, v := range *(*writers_t)(atomic.LoadPointer(&self.out[LOG_DEBUG.Levels[0]])) {
-		v.WriteLevel(ContextStore(ctx, LOG_DEBUG.Name, format, args...), format, args...)
+		v.WriteLevel(level, format, args...)
 	}
 }
 
 func (self *log_t) TraceCtx(ctx context.Context, format string, args ...interface{}) {
+	level := ContextStore(ctx, LOG_TRACE.Name, format, args...)
 	for _, v := range *(*writers_t)(atomic.LoadPointer(&self.out[LOG_TRACE.Levels[0]])) {
-		v.WriteLevel(ContextStore(ctx, LOG_TRACE.Name, format, args...), format, args...)
+		v.WriteLevel(level, format, args...)
 	}
 }
 
