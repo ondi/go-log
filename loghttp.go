3dc04761b352fc21c1f123e42559a420a1804343
ondi <thy321@gmail.com>
2023-04-28 15:14:15 +0300

    minor fix

--- loghttp.go
+++ loghttp.go
@@ -77,11 +77,11 @@ func DefaultTransport(timeout time.Duration, MaxIdleConns int, MaxIdleConnsPerHo
 		ForceAttemptHTTP2:   true,
 		MaxIdleConns:        MaxIdleConns,
 		MaxIdleConnsPerHost: MaxIdleConnsPerHost,
-		TLSHandshakeTimeout: timeout,
+		// TLSHandshakeTimeout: timeout,
 		// IdleConnTimeout:       timeout,
-		ResponseHeaderTimeout: timeout,
-		ExpectContinueTimeout: timeout,
-		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
+		// ResponseHeaderTimeout: timeout,
+		// ExpectContinueTimeout: timeout,
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 }
 
